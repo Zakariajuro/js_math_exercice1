c717f95cb799f615d6174098db4612187558be88
Zakaria <zakPro@live.be>
2020-12-30 11:49:29 +0100

    exo-fini

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+let nombre = 8.56
+// nombre = Math.round(nombre);
+
+// nombre = (Math.ceil(8.56));
+
+nombre = Math.floor(8.56);
+
+// nombre = Math.round(Math.random());
+
+// nombre = Math.round(Math.random()* 100);
+
+// nombre = Math.pow(8, 2);
+
+// nombre = Math.sqrt(9);
+ 
+// nombre = Math.abs(-1);
+
+// nombre = Math.max(-2, 1000, 8, 57);
+
+// nombre = Math.min(-2, 1000, 8, 57);
+
+
+
+console.log(nombre);
